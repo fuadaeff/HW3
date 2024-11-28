e5eaedbe751a51d8f6b8293e0944da2c7a14d9bd
fuadaeff <fabdulla057@gmail.com>
2024-11-28 19:12:22 +0400

    Add files via upload

---
+++ Sumof1-10/Sumof1-10/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Sumof1_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int S = 0;
+            for(int i = 1; i <= 10; i++)
+            {
+                S += i;
+            }
+            Console.WriteLine(S);
+        }
+    }
+}
