478a893764c09c89206edfc7571bacd787612613
fuadaeff <fabdulla057@gmail.com>
2024-11-28 19:13:13 +0400

    Add files via upload

---
+++ ifNegativeStop/ifNegativeStop/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace ifNegativeStop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int S= 0, number = 0;
+            while (number >= 0)
+            {
+                Console.WriteLine("Enter a number:");
+                number = int.Parse(Console.ReadLine());
+                if (number >= 0)
+                {
+                    S += number;
+                }
+            }
+            Console.WriteLine(S);
+            // sertde menfi oldugun yoxlayaraqda yaza bilerik:
+            //int S = 0, number = 0;
+            //while (number >= 0)
+            //{
+            //    Console.WriteLine("Enter a number:");
+            //    number = int.Parse(Console.ReadLine());
+            //    if (number < 0)
+            //    {
+            //        break;
+            //    }
+            //    else
+            //    {
+            //        S += number;
+            //    }
+            //}
+            //Console.WriteLine(S);
+        }
+    }
+}
