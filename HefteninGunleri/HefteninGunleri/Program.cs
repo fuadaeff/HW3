873bd2b1fc6432710f5323128784b7cedc871d57
fuadaeff <fabdulla057@gmail.com>
2024-11-28 19:09:52 +0400

    Add files via upload

---
+++ HefteninGunleri/HefteninGunleri/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace HefteninGunleri
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Gun daxil edin:");
+            int day = int.Parse(Console.ReadLine());
+            switch (day) {
+                case 1:
+                    Console.WriteLine("Bazar Ertesi.");
+                    break;
+                case 2:
+                    Console.WriteLine("Cersenbe Axsami.");
+                    break;
+                case 3:
+                    Console.WriteLine("Cersenbe.");
+                    break;
+                case 4:
+                    Console.WriteLine("Cume Axsami");
+                    break;
+                case 5:
+                    Console.WriteLine("Cume");
+                    break;
+                case 6:
+                    Console.WriteLine("Senbe");
+                    break;
+                case 7:
+                    Console.WriteLine("Bazar");
+                    break;
+                default:
+                    Console.WriteLine("1 ve 7 arasi eded daxil etmelisiniz!");
+                    break;
+            
+            
+            }
+        }
+    }
+}
